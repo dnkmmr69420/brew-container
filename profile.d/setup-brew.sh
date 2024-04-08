6f8f70265e97402ddb917b6040e88c16c4fab79f
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2024-04-08 09:09:57 -0500

    Update setup-brew.sh

--- profile.d/setup-brew.sh
+++ profile.d/setup-brew.sh
@@ -1,5 +1,6 @@
 if test ! -f /etc/brew-setup.conf; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/brew-container/main/profile.d/brew.sh
 fi
 
 sudo touch /etc/brew.setup.conf
